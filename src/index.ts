933a5c4572c6d6dcedf559ba7250bb0732c7031b
Daniel David <danielalejandro.2000@outlook.com>
2021-02-17 13:47:12 -0600

    CRUD operations using graphql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Issue } from './entities/Issue';
@@ -6,6 +7,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql'
 import { HelloResolver } from './resolvers/hello';
+import { IssueResolver } from './resolvers/issue';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -14,9 +16,10 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, IssueResolver],
       validate: false
-    })
+    }),
+    context: () => ({ em: orm.em })
   });
 
   apolloServer.applyMiddleware({ app });
---
+++ src/resolvers/issue.ts
@@ -0,0 +1,55 @@
+import { Issue } from '../entities/Issue';
+import { MyContext } from '../types';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql'; 
+
+@Resolver()
+export class IssueResolver {
+  @Query(() => [Issue])
+  issues( @Ctx() { em }: MyContext): Promise<Issue[]> {
+    return em.find(Issue, {});
+  }
+
+  @Query(() => Issue, { nullable: true })
+  issue( 
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Issue | null> {
+    return em.findOne(Issue, { id });
+  }
+
+  @Mutation(() => Issue)
+  async createIssue( 
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Issue> {
+    const issue = em.create(Issue, { title })
+    await em.persistAndFlush(issue);
+    return issue;
+  }
+
+  @Mutation(() => Issue)
+  async updateIssue( 
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Issue | null> {
+    const issue = await em.findOne(Issue, { id });
+    if (!issue) {
+      return null;
+    }
+    if (typeof title !== undefined) {
+      issue.title = title;
+      await em.persistAndFlush(issue)
+    }
+    return issue;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteIssue( 
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Issue, { id });
+    return true;
+  }
+}
