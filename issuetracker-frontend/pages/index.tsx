40b5a9ed502b0fac64cc32af35af06cce6eabadf
Daniel David <danielalejandro.2000@outlook.com>
2021-02-19 23:12:49 -0600

    Next JS with Semantic UI

---
+++ issuetracker-frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Modal, Button, Icon } from 'semantic-ui-react'
+
+export default function Home() {
+  return (
+    <div>Hello World
+      <Modal>
+        e
+      </Modal>
+    </div>
+  )
+}
